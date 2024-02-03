43dc041cd17197de214ad8feb9ec275646941bfe
SarkerIT <sarker.data@gmail.com>
2024-02-03 01:05:22 -0600

    object intro

---
+++ intro-js/object.js
@@ -0,0 +1,15 @@
+let myObj = {
+  name: "John Doe",
+  age: 25,
+  city: "Oklahoma City",
+  isStudent: true,
+};
+
+// console.log(myObj);
+
+let key = "";
+
+for (let keys in myObj) {
+  key += keys + " ";
+  console.log(`key: ${keys} | type: ${typeof keys}`);
+}
