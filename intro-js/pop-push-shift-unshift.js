819638fe7448c1e1bc4a5d2290363a5c458b2521
SarkerIT <sarker.data@gmail.com>
2024-02-03 21:28:01 -0600

    push-pop shift-unshift

---
+++ intro-js/pop-push-shift-unshift.js
@@ -0,0 +1,22 @@
+let shoppingList = [
+  "pencil",
+  "notebook",
+  "eraser",
+  "pen",
+  "marker",
+  "permanent marker",
+  "paper-clip",
+];
+
+console.log(shoppingList);
+
+shoppingList.pop();
+console.log(shoppingList);
+
+shoppingList.push("binder");
+console.log(shoppingList);
+
+shoppingList.shift();
+console.log(shoppingList);
+shoppingList.unshift("Dry-erase marker");
+console.log(shoppingList);
